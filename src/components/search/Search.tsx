3a665d2e23bd79a8ca77e8c74d3de86841a3e8d4
Vladyslav Proshchavaiev <f3joule@gmail.com>
2019-11-11 23:09:18 +0200

    Add filter results by current tab

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,68 +1,22 @@
 import React from 'react';
 import { DataSearch } from '@appbaseio/reactivesearch';
-// import ViewBlog from '../blogs/ViewBlog';
-// import { ViewPost } from '../posts/ViewPost';
 import Router from 'next/router';
-// import ViewProfile from '../profiles/ViewProfile';
 
 const App = () => {
-  // const getPreviewComponent = (index: string, id: string) => {
-  //   switch (index) {
-  //     case 'subsocial_blogs':
-  //       return <ViewBlog key={id} id={id} nameOnly />;
-  //     case 'subsocial_posts':
-  //       return <ViewPost key={id} id={id} nameOnly />;
-  //     case 'subsocial_profiles':
-  //       return <ViewProfile key={id} id={id} nameOnly />;
-  //     default:
-  //       return <ViewBlog key={id} id={id} nameOnly />;
-  //   }
-  // };
-
   return (
-    // <>
-    //   <ReactiveComponent
-    //     componentId='search'
-    //     render={(loading) => {
-    //       return (
-    //         <div className={'ui ' + (loading === true ? 'loading' : '') + ' search DfSearch'}>
-    //           <div className='ui icon input'>
-    //             <input className='prompt' type='text' placeholder='Search for data' />
-    //             <i className='search icon'></i>
-    //           </div>
-    //           <div className='results'>
-    //             <a className='result'>
-    //               <div className='content'>
-    //                 <div className='title'>Blog</div>
-    //               </div>
-    //             </a>
-    //           </div>
-    //         </div>
-    //       );
-    //     }}
-    //   />
-    <DataSearch
-      componentId='q'
-      dataField={['name', 'desc', 'title', 'body', 'username', 'fullname', 'about']}
-      fieldWeights={[2, 1, 2, 1, 2, 2, 1]}
-      URLParams
-      // parseSuggestion={(data) => ({
-      //   label: (
-      //     <div>{getPreviewComponent(data.source._index, data.source._id)}</div>
-      //   ),
-      //   value: data.source.name,
-      //   source: data.source
-      // })}
-      onValueSelected={(value) => Router.push(`/search?q=${value}`)}
-
-      placeholder='Search for data'
-      iconPosition='right'
-      className='DfSearch'
-      innerClass={{
-        list: 'visible menu transition'
-      }}
-    />
-    // </>
+    <>
+      <DataSearch
+        componentId='q'
+        dataField={['name', 'desc', 'title', 'body', 'username', 'fullname', 'about']}
+        fieldWeights={[2, 1, 2, 1, 2, 2, 1]}
+        URLParams
+        onValueSelected={(value) => Router.push(`/search?q=${value}`)}
+        placeholder='Search Subsocial'
+        iconPosition='right'
+        className='DfSearch'
+        innerClass={{ list: 'visible menu transition' }}
+      />
+    </>
   );
 };
 
--- src/components/search/SearchResults.tsx
+++ src/components/search/SearchResults.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { ReactiveList } from '@appbaseio/reactivesearch';
+import React, { useState } from 'react';
+import { ReactiveList, ReactiveComponent } from '@appbaseio/reactivesearch';
 import ViewBlog from '../blogs/ViewBlog';
 import { ViewPost } from '../posts/ViewPost';
 import Section from '../utils/Section';
-import { Tab } from 'semantic-ui-react';
+import { Tab, StrictTabProps } from 'semantic-ui-react';
 import ViewProfile from '../profiles/ViewProfile';
 import { ElasticIndex } from './ElasticConfig';
 
@@ -12,27 +12,84 @@ type DataResults = {
   _index: string;
 };
 
-const renderResults = (results: DataResults[]) => {
-  const filterResults = (indexName: string) => results.filter(res => res._index === indexName);
+const AllIndexes = '*';
 
-  const renderBlogs = () =>
-    filterResults(ElasticIndex.blogs).map((res, i) => <ViewBlog key={i} id={res._id} previewDetails withFollowButton />);
+type Props = {
+  results: DataResults[]
+}
 
-  const renderPosts = () =>
-    filterResults(ElasticIndex.posts).map((res, i) => <ViewPost key={i} id={res._id} preview withLink={true} />);
+type OnTabChangeFn = (event: React.MouseEvent<HTMLDivElement>, data: StrictTabProps) => void;
 
-  const renderProfiles = () =>
-    filterResults(ElasticIndex.profiles).map((res, i) => <ViewProfile key={i} id={res._id} preview />);
+const TabsAndResults = (props: Props) => {
+  const { results } = props;
+  const [activeIndex, setActiveIndex] = useState(AllIndexes);
 
+  const handleTabChange: OnTabChangeFn = (e, data) => {
+    if (!data || !data.panes) return;
+
+    const activeTab = data.panes[data.activeIndex as number];
+    const indexName = (activeTab as unknown as { key: string }).key;
+    setActiveIndex(indexName)
+  }
+
+  const Previews = () => !results || !results.length
+    ? <em>No results found</em>
+    : <div>{results.map((res, i) => {
+      switch (res._index) {
+        case ElasticIndex.blogs:
+          return <ViewBlog key={i} id={res._id} previewDetails withFollowButton />;
+        case ElasticIndex.posts:
+          return <ViewPost key={i} id={res._id} preview withLink={true} />;
+        case ElasticIndex.profiles:
+          return <ViewProfile key={i} id={res._id} preview />;
+        default:
+          return null;
+      }
+    })}</div>
+
+  const TabContent = () =>
+    <Tab.Pane><Previews /></Tab.Pane>
 
   const panes = [
-    { key: 'all', menuItem: 'All', render: () => <Tab.Pane>{[renderBlogs(), renderPosts(), renderProfiles()]}</Tab.Pane> },
-    { key: 'blogs', menuItem: 'Blogs', render: () => <Tab.Pane>{renderBlogs()}</Tab.Pane> },
-    { key: 'posts', menuItem: 'Posts', render: () => <Tab.Pane>{renderPosts()}</Tab.Pane> },
-    { key: 'profiles', menuItem: 'Profiles', render: () => <Tab.Pane>{renderProfiles()}</Tab.Pane> }
+    {
+      key: AllIndexes,
+      menuItem: 'All',
+      render: () => <TabContent />
+    },
+    {
+      key: ElasticIndex.blogs,
+      menuItem: 'Blogs',
+      render: () => <TabContent />
+    },
+    {
+      key: ElasticIndex.posts,
+      menuItem: 'Posts',
+      render: () => <TabContent />
+    },
+    {
+      key: ElasticIndex.profiles,
+      menuItem: 'Profiles',
+      render: () => <TabContent />
+    }
   ];
 
-  return <Tab panes={panes} />;
+  return <>
+    <Tab panes={panes} onTabChange={handleTabChange} />
+    <ReactiveComponent
+      componentId='tab'
+      customQuery={() => {
+        return activeIndex === AllIndexes
+          ? null
+          : {
+            query: {
+              term: {
+                _index: activeIndex
+              }
+            }
+          };
+      }}
+    />
+  </>;
 };
 
 const App = () => {
@@ -41,15 +98,14 @@ const App = () => {
       <ReactiveList
         componentId='page'
         dataField='id'
-        react={{
-          and: ['q']
-        }}
+        react={{ and: ['q', 'tab'] }}
         showResultStats={false}
         URLParams={true}
-        size={10}
-        pages={3}
+        size={20}
+        pages={100}
         pagination={true}
-        render={res => renderResults(res.data)}
+        render={res => <TabsAndResults results={res.data} />}
+        renderNoResults={() => null}
       />
     </Section>
   );
