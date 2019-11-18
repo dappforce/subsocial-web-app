5f13fd7aa41286f67338c612c3b6617eb51f000d
OLeh <oleh.melnechyk@gmail.com>
2019-11-18 18:21:35 +0200

    Add hack for build

---
+++ before-build-hack.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+> ./node_modules/@polkadot/ui-app/Button/Button.css
+> ./node_modules/@polkadot/ui-app/Editor/style.css
+> ./node_modules/@polkadot/ui-params/Params.css
+> ./node_modules/@polkadot/ui-app/InputAddress/InputAddress.css
+> ./node_modules/@polkadot/ui-app/InputExtrinsic/InputExtrinsic.css
