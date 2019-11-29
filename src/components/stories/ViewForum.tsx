d8a95343f94c1c86cf12e0bea6feee18a38d8c64
Lera <SsickSsadWworkd@gmail.com>
2019-11-29 22:35:37 +0200

    Fix cascader

--- src/components/stories/ViewForum.tsx
+++ src/components/stories/ViewForum.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Icon, Button, Cascader } from 'antd';
 import './style.css';
 import { Category, TopicData } from './types';
 import ListForumTopics from './ListForumTopics';
+import RadioButton from 'antd/lib/radio/radioButton';
 
 type ForumProps = {
   categoryList: Category[],
@@ -12,25 +13,27 @@ type ForumProps = {
 function ViewForum (props: ForumProps) {
 
   const { categoryList, data } = props;
+
   return (
     <>
       <div className='ForumHeader'>
         <div className='Navigation'>
           <a href=''><Icon type='home' theme='twoTone'/> / </a>
           <Cascader
+            fieldNames={{ label: 'category', value: 'category', children: 'children' }}
             options={categoryList}
           />
         </div>
         <div className='Sorting'>
-          <div style={{ marginRight: '1.5rem' }}>Sort by:</div>
-          <div className='Latest Button'>
+          <div style={{ marginRight: '1.5rem', paddingTop: '.2rem' }}>Sort by:</div>
+          <Button className='Latest Button' type='link'>
             <Icon type='clock-circle'/>
             <div>Latest</div>
-          </div>
-          <div className='Score Button'>
-            <Icon type='clock-circle'/>
+          </Button>
+          <Button className='Score Button' type='link'>
+            <Icon type='rise'/>
             <div>Score</div>
-          </div>
+          </Button>
         </div>
         <div className='Settings'>
           <Button type='primary' icon='plus'>New Topic</Button>
