e74b216e10eb85c2c9ecab09491fd6b8eb023144
sanja <choimerovingian@gmail.com>
2020-02-20 18:11:55 +0200

    empty space

--- src/components/blogs/ViewBlogById.tsx
+++ src/components/blogs/ViewBlogById.tsx
@@ -7,7 +7,6 @@ import { useRouter } from 'next/router';
 const Component = () => {
   const router = useRouter();
   const { blogId } = router.query;
-
   return blogId
     ? <ViewBlog id={new BlogId(blogId as string)} />
     : null;
