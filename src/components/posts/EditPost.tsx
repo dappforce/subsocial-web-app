6e5b130145114fc06fba47245ef880629a8ed0a1
sanja <choimerovingian@gmail.com>
2020-03-05 14:15:18 +0200

    blogId fix

--- src/components/posts/EditPost.tsx
+++ src/components/posts/EditPost.tsx
@@ -100,8 +100,10 @@ const InnerForm = (props: FormProps) => {
     tags
   } = values;
 
+  const preparedBlogId = struct?.blog_id.toString() || blogId?.toString()
+
   const goToView = (id: PostId) => {
-    Router.push(`/blogs/${blogId?.toString()}/posts/${id}`).catch(console.log);
+    Router.push(`/blogs/${preparedBlogId}/posts/${id}`).catch(console.log);
   };
 
   const [ ipfsHash, setIpfsCid ] = useState('');
@@ -202,15 +204,8 @@ const InnerForm = (props: FormProps) => {
     ? renderTxButton()
     : <>
       <Breadcrumb>
-        <Breadcrumb.Item>
-          <a href="/">Home</a>
-        </Breadcrumb.Item>
-        <Breadcrumb.Item>
-          <a href={`/blogs/${blogId?.toString()}`}>My Blog</a>
-        </Breadcrumb.Item>
-        <Breadcrumb.Item>
-          Edit Post
-        </Breadcrumb.Item>
+        <Breadcrumb.Item><a href={`/blogs/${preparedBlogId}`}>My Blog</a></Breadcrumb.Item>
+        <Breadcrumb.Item>Edit Post</Breadcrumb.Item>
       </Breadcrumb>
       <HeadMeta title={sectionTitle}/>
       <Section className='EditEntityBox' title={sectionTitle}>
