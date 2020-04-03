7c618e511a10bc110f8b836b2fecc1c9f0c8715a
Alert <oleh.melnechyk@gmail.com>
2020-04-03 16:33:07 +0300

    Fix env

--- public/env.js
+++ public/env.js
@@ -4,10 +4,10 @@ if (!window.process) window.process = {};
 if (!window.process.ENV) window.process.ENV = {};
 
 window.process.env = {
-  SUBSTRATE_URL: 'ws://testnet.subsocial.network:9944',
-  ELASTIC_URL: 'http://testnet.subsocial.network:9200',
+  SUBSTRATE_URL: 'ws://127.0.0.1:9944',
+  ELASTIC_URL: 'http://127.0.0.1:9200',
   OFFCHAIN_URL: 'http://127.0.0.1:3001',
   OFFCHAIN_WS: 'ws://127.0.0.1:3011',
-  APPS_URL: 'http://testnet.subsocial.network:3002',
-  IPFS_URL: 'http://testnet.subsocial.network:3001'
+  APPS_URL: 'http://127.0.0.1:3002',
+  IPFS_URL: '/ip4/127.0.0.1/tcp/5001'
 };
