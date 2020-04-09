053a6528ec8c02f8bdbb95581cb1b8cb83d739cd
Alert <oleh.melnechyk@gmail.com>
2020-04-09 18:47:51 +0300

    Remove copy paste

--- run-dev.sh
+++ run-dev.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 ./fix-polkadot-libs.sh
-node export-env.js
 yarn run dev
--- src/components/utils/OffchainUtils.ts
+++ src/components/utils/OffchainUtils.ts
@@ -8,21 +8,25 @@ export const ipfsUrl = getEnv('IPFS_URL') || '/ip4/127.0.0.1/tcp/5001/http';
 export const ipfs = new SubsocialIpfsApi({ connect: ipfsUrl, offchainUrl });
 export const offchainWs = getEnv('OFFCHAIN_WS')
 
+function getOffchainUrl (subUrl: string): string {
+  return `${offchainUrl}/v1/offchain/${subUrl}`
+}
+
 export const getNewsFeed = async (myAddress: string, offset: number, limit: number): Promise<Activity[]> => {
-  const res = await axios.get(`${offchainUrl}/v1/offchain/feed/${myAddress}?offset=${offset}&limit=${limit}`);
+  const res = await axios.get(getOffchainUrl(`/feed/${myAddress}?offset=${offset}&limit=${limit}`));
   const { data } = res;
   return data;
 };
 
 export const getNotifications = async (myAddress: string, offset: number, limit: number): Promise<Activity[]> => {
-  const res = await axios.get(`${offchainUrl}/v1/offchain/notifications/${myAddress}?offset=${offset}&limit=${limit}`);
+  const res = await axios.get(getOffchainUrl(`/notifications/${myAddress}?offset=${offset}&limit=${limit}`));
   const { data } = res;
   return data;
 };
 
 export const clearNotifications = async (myAddress: string): Promise<void> => {
   try {
-    const res = await axios.post(`${offchainUrl}/v1/offchain/notifications/${myAddress}/readAll`);
+    const res = await axios.post(getOffchainUrl(`/notifications/${myAddress}/readAll`));
 
     if (res.status !== 200) {
       console.warn('Failed to mark all notifications as read for account:', myAddress, 'res.status:', res.status)
