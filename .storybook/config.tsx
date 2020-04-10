ead3f21467d71bdd304f49a077f7986fc4c8a4b8
Alex Siman <aleksandr.siman@gmail.com>
2020-04-10 16:17:02 +0300

    Mock Next.js router in Storybook

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import { configure, addDecorator } from '@storybook/react';
 import '@storybook/addon-console';
+// @ts-ignore
 import StoryRouter from 'storybook-react-router';
+import { RouterContext } from 'next/dist/next-server/lib/router-context';
+import { mockNextRouter } from '../src/stories/mockNextRouter';
+import { withStorybookContext } from '../src/stories/withStorybookContext';
 
 import '../src/components/utils/styles';
 import './style.css';
 
+// Mock React router:
 addDecorator(StoryRouter());
+
+// Mock Next.js router
+addDecorator(story => (
+  <RouterContext.Provider value={mockNextRouter}>
+    {story()}
+  </RouterContext.Provider>
+))
+
+// Mock Substrate TxButton:
+addDecorator(withStorybookContext)
 
 addDecorator(story => (
   <div className='StorybookRoot'>
     {story()}
   </div>
-));
+))
 
 configure(require.context('../src', true, /\.stories\.tsx?$/), module)
