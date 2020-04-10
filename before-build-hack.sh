0c5a8c23651e157b6b6cca4a5d4e12ebb87daa91
Alex Siman <aleksandr.siman@gmail.com>
2020-04-10 11:10:43 +0300

    Patch polkadot deps and export .env on more yarn scripts

--- before-build-hack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-> ./node_modules/@polkadot/ui-assets/empty.svg
-> ./node_modules/@polkadot/ui-assets/chains/kusama-128.gif
-> ./node_modules/@polkadot/ui-assets/centrifuge.png
-> ./node_modules/@polkadot/ui-assets/edgeware-circle.svg
-> ./node_modules/@polkadot/ui-assets/polkadot-circle.svg
-> ./node_modules/@polkadot/ui-assets/polkadot-js.svg
-> ./node_modules/@polkadot/ui-assets/substrate-hexagon.svg
