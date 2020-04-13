89255891813e2e42b017f2827eeac890787564ad
Alex Siman <aleksandr.siman@gmail.com>
2020-04-13 22:01:18 +0300

    Delete unused imports on _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import App from 'next/app';
 import Head from 'next/head';
 import MainPage from '../layout/MainPage'
 import SidebarCollapsedProvider from '../components/utils/SideBarCollapsedContext';
-import { getApi, ipfsUrl, offchainUrl } from '../components/utils/SubsocialConnect';
-import { SubsocialApi } from '@subsocial/api/fullApi';
 
 function MyApp (props) {
   const { Component, pageProps } = props
