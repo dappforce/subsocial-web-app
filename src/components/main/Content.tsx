74517d328368a8a2c8518d27167885ebe3881396
OLeh MeLL <oleh.melnechyk@gmail.com>
2020-04-21 17:13:32 +0300

    Update libs

--- src/components/main/Content.tsx
+++ src/components/main/Content.tsx
@@ -1,18 +1,9 @@
-import React, { useContext } from 'react'
-import { StatusContext } from '@polkadot/react-components'
+import React from 'react'
 import { Navigation } from '../../layout/Navigation'
-import dynamic from 'next/dynamic'
 
-const Status = dynamic(() => import('./Status'), { ssr: false });
 export const Content: React.FunctionComponent = ({ children }) => {
-  const { queueAction, stqueue, txqueue } = useContext(StatusContext);
 
   return <>
-    <Status
-      queueAction={queueAction}
-      stqueue={stqueue}
-      txqueue={txqueue}
-    />
     <Navigation>
       {children}
     </Navigation>
