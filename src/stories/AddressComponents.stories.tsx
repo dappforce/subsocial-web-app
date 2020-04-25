dda3b3e239f28e8a5a1f17c820c86cff64c303a2
Alex Siman <aleksandr.siman@gmail.com>
2020-04-25 11:02:59 +0300

    Fix notifications story

--- src/stories/AddressComponents.stories.tsx
+++ src/stories/AddressComponents.stories.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { AuthorPreview, ProfilePreview, AddressPopup } from '../components/profiles/address-views';
 import { mockProfileDataAlice } from './mocks/SocialProfileMocks';
 import { mockAccountAlice } from './mocks/AccountMocks'
+
 export default {
-  title: 'Profile | Components'
+  title: 'Profiles | Previews'
 }
 
 export const _AuthorPreview = () =>
-    <AuthorPreview address={mockAccountAlice.toString()} owner={mockProfileDataAlice} details={<>{new Date().toLocaleString()}</>}/>
+  <AuthorPreview address={mockAccountAlice.toString()} owner={mockProfileDataAlice} details={<>{new Date().toLocaleString()}</>}/>
 
 export const _ProfilePreview = () =>
-    <ProfilePreview owner={mockProfileDataAlice} address={mockAccountAlice.toString()}/>
+  <ProfilePreview owner={mockProfileDataAlice} address={mockAccountAlice.toString()}/>
 
 export const _ProfilePreviewMini = () =>
-    <ProfilePreview owner={mockProfileDataAlice} address={mockAccountAlice.toString()} mini/>
+  <ProfilePreview owner={mockProfileDataAlice} address={mockAccountAlice.toString()} mini/>
 
 export const __AddressPopup = () =>
-    <AddressPopup owner={mockProfileDataAlice} address={mockAccountAlice.toString()} />
+  <AddressPopup owner={mockProfileDataAlice} address={mockAccountAlice.toString()} />
--- src/stories/Notifications.stories.tsx
+++ src/stories/Notifications.stories.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Notification } from '../components/activity/NewNotifiation';
+import { Notification } from '../components/activity/Notification';
 import { mockProfileDataAlice } from './mocks/SocialProfileMocks';
 import { mockAccountAlice } from './mocks/AccountMocks'
 import { mockBlogDataAlice } from './mocks/BlogMocks'
 import { ViewBlogPage } from '../components/blogs/ViewBlog'
+
 export default {
-  title: 'Notification'
+  title: 'Activity | Notifications'
 }
 
-export const _NotificationTest = () =>
-    <Notification address={mockAccountAlice.toString()} owner={mockProfileDataAlice} details={new Date().toLocaleString()} textActivity={<>and 1 people use here notification <ViewBlogPage blogData={mockBlogDataAlice} nameOnly withLink /></>}/>
-
+export const _MyNotifications = () =>
+  <Notification address={mockAccountAlice.toString()} owner={mockProfileDataAlice} details={new Date().toLocaleString()} textActivity={<>and 1 people use here notification <ViewBlogPage blogData={mockBlogDataAlice} nameOnly withLink /></>}/>
