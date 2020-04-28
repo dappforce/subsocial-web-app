5620f91877fd3db7ea5fca915e60ec20d3b3aac5
OLeh MeLL <oleh.melnechyk@gmail.com>
2020-04-28 17:52:23 +0300

    Fix double text in NameDetails

--- src/components/profiles/address-views/utils/NameDetails.tsx
+++ src/components/profiles/address-views/utils/NameDetails.tsx
@@ -4,16 +4,30 @@ import { toShortAddress } from '@polkadot/react-components/util';
 import Link from 'next/link';
 import { ProfileData } from '@subsocial/types';
 import { InfoDetails } from '../AuthorPreview';
+import { nonEmptyStr } from '@subsocial/utils';
 
 export const NameDetails: React.FunctionComponent<AddressProps> = ({ owner = {} as ProfileData, address }) => {
   const shortAddress = toShortAddress(address);
   const { profile, content, struct } = owner
+  let title = ''
+  let subtitle = ''
+
+  if (content && nonEmptyStr(content.fullname)) {
+    title = content?.fullname
+    subtitle = nonEmptyStr(profile?.username) ? `@${profile?.username} - ${shortAddress}` : shortAddress
+  } else if (nonEmptyStr(profile?.username)) {
+    title = `@${profile?.username}`
+    subtitle = shortAddress
+  } else {
+    title = shortAddress
+  }
+
   return (
     <Link href='/profile/[address]' as={`/profile/${address}`}>
       <a className='ui--AddressComponents-address'>
-        <span className='AddressComponents-fullname'>{content?.fullname || shortAddress}</span>
+        <span className='AddressComponents-fullname'>{title}</span>
         <InfoDetails address={address} details={<>Repupation: {struct.reputation.toString()}</>} />
-        <div className='DfPopup-username'>{profile?.username && `${profile?.username} - `}{shortAddress}</div>
+        nonEmptyStr(subTitle) <div className='DfPopup-username'>{subtitle}</div>
       </a>
     </Link>
   );
