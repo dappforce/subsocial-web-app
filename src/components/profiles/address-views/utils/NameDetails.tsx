2ab9ba81c9283575ed916f8ba4f44b891851f5d9
OLeh MeLL <oleh.melnechyk@gmail.com>
2020-04-28 18:27:29 +0300

    Fix syntax error

--- src/components/profiles/address-views/utils/NameDetails.tsx
+++ src/components/profiles/address-views/utils/NameDetails.tsx
@@ -26,8 +26,8 @@ export const NameDetails: React.FunctionComponent<AddressProps> = ({ owner = {} 
     <Link href='/profile/[address]' as={`/profile/${address}`}>
       <a className='ui--AddressComponents-address'>
         <span className='AddressComponents-fullname'>{title}</span>
-        nonEmptyStr(subTitle) <div className='DfPopup-username'>{subtitle}</div>
-        <InfoDetails address={address} details={<>Repupation: {struct.reputation.toString()}</>} />
+        <div className='DfPopup-username'>{subtitle}</div>
+        {nonEmptyStr(subtitle) && <InfoDetails address={address} details={<>Repupation: {struct.reputation.toString()}</>} />}
       </a>
     </Link>
   );
