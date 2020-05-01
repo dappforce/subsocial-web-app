115c080013b64eb7759feffd955a30d8ca30cad2
OLeh MeLL <oleh.melnechyk@gmail.com>
2020-05-01 12:31:15 +0300

    Fix show modals

--- src/components/profiles/address-views/utils/SelectAccount.tsx
+++ src/components/profiles/address-views/utils/SelectAccount.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import InputAddress from '../../../utils/InputAddress';
 import { u32 } from '@polkadot/types'
 import BN from 'bn.js'
+import { ZERO } from '../../../utils';
 
 type Props = {
   reputation: BN | u32 | string | number
 }
 
-export const SelectAccount: React.FunctionComponent<Props> = ({ reputation }) => {
+export const SelectAccount: React.FunctionComponent<Props> = ({ reputation = ZERO }) => {
   return <div className='DfSelectAccount'>
     <div className='DfProfileInfo'>
         My reputation: {reputation.toString()}
