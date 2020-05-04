09ac8b05d9f0dece70981efc13b2e482bf56f6e7
OLeh MeLL <oleh.melnechyk@gmail.com>
2020-05-04 15:17:24 +0300

    WIP: create new comments components

---
+++ src/components/posts/CommentsTree.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { ExtendedPostData } from '@subsocial/types';
+import { PostId } from '@subsocial/types/substrate/interfaces'
+import ListData from '../utils/DataList';
+import { ViewComment } from './NewViewComment';
+import { EditComment } from './NewEditComment';
+import Section from '../utils/Section';
+import { getProfileName } from '../profiles/address-views/Name';
+import mdToText from 'markdown-to-txt';
+import { HeadMeta } from '../utils/HeadMeta';
+
+type CommentsTreeProps = {
+    comments: ExtendedPostData[]
+}
+
+const CommentsTree: React.FunctionComponent<CommentsTreeProps> = ({ comments }) => {
+    return <ListData
+    dataSource={comments}
+    renderItem={(item) => {
+      const { post: { struct, content }, owner } = item;
+      const address = struct.created.account.toString();
+
+      return <ViewComment key={struct.id.toString()} address={address} struct={struct} content={content} owner={owner} />
+    }}
+  />
+}
+
+type CommentSectionProps = CommentsTreeProps & {
+  rootId: PostId
+}
+
+const CommentSection: React.FunctionComponent<CommentSectionProps> = ({ comments, rootId }) => (
+  <div className='DfCommentSection'>
+    <EditComment parentId={rootId} />
+    <CommentsTree comments={comments} />
+  </div>
+)
+
+type CommentPageProps = {
+  comment: ExtendedPostData
+}
+
+const CommentPage: React.FunctionComponent<CommentPageProps> = ({ comment }) => {
+  const { post: { struct, content }, owner } = comment;
+  const address = struct.created.account.toString()
+  const profileName = getProfileName({ address, owner }).toString()
+
+  return <Section className='DfContentPage DfEntirePost'>
+      <HeadMeta desc={mdToText(content?.body)} title={`${profileName} commented on ${content?.title}`} />
+      
+  </Section> 
+
+}
