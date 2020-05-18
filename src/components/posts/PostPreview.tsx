a3def2c2fe99151656f58044cd1b0a1371266fdd
olehmell <oleh.melnechyk@gmail.com>
2020-05-18 14:36:53 +0300

    Dirty fix for build

--- src/components/posts/PostPreview.tsx
+++ src/components/posts/PostPreview.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ViewPostPage from '../posts/ViewPost';
-import { ExtendedPostData, BlogData } from '@subsocial/types';
+import { Loading } from '../utils/utils';
+import { useSubsocialApi } from '../utils/SubsocialApiContext';
+import { ExtendedPostData } from '@subsocial/types';
+import { Blog } from '@subsocial/types/substrate/interfaces';
 
 type PostPreviewProps = {
   post: ExtendedPostData
@@ -9,7 +12,7 @@ type PostPreviewProps = {
 export function PostPreview (props: PostPreviewProps) {
   const { post, ext, owner, blog = {} as BlogData } = props.post
 
-  return <ViewPostPage postData={post} blog={blog.struct} postExtData={ext} owner={owner} variant='preview' withBlogName />
+  return <ViewPostPage postData={post} blog={{} as Blog} postExtData={ext} owner={owner} variant='preview' withBlogName />
 }
 
 export default PostPreview
