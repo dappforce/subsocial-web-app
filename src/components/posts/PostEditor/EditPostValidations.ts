44e6498b1180a940b00b9f8171adf18cf3009a55
Alex Siman <aleksandr.siman@gmail.com>
2020-05-22 19:22:26 +0300

    Refactor EditPostValidations

--- src/components/posts/PostEditor/EditPostValidations.ts
+++ src/components/posts/PostEditor/EditPostValidations.ts
@@ -1,20 +1,28 @@
 import * as Yup from 'yup';
+import { urlValidation, minLenError, maxLenError } from '../../utils/forms/validation';
+import { pluralize } from '@subsocial/utils';
 
-export type ValidationProps = {
-  // TODO this should be a UI const.
-}
+const MAX_TAGS = 10
 
-const MAX_TAGS_PER_POST = 10
-const MIN_TEXT_BLOCK_LENGTH = 1
 const MIN_BLOCKS = 1
+const MAX_BLOCKS = 100
+
+const TEXT_BLOCK_MIN_LEN = 1
+const TEXT_BLOCK_MAX_LEN = 100 * 1024 // 100 KB
+
+const TextBlockSchema = Yup.string()
+  .min(TEXT_BLOCK_MIN_LEN, minLenError('Text block', TEXT_BLOCK_MIN_LEN))
+  .max(TEXT_BLOCK_MAX_LEN, maxLenError('Text block', TEXT_BLOCK_MAX_LEN))
 
 const buildSchema = () => Yup.object().shape({
   title: Yup.string()
     .required('Post title is required'),
+
   tags: Yup.array()
-    .max(MAX_TAGS_PER_POST, `Too many tags. Maximum: ${MAX_TAGS_PER_POST}`),
-  canonical: Yup.string()
-    .url('Canonical must be a valid URL.'),
+    .max(MAX_TAGS, `Post can have ${pluralize(MAX_TAGS, 'tag')} max.`),
+
+  canonical: urlValidation('Original post URL'),
+
   blockValues: Yup.array()
     .of(
       Yup.object({
@@ -22,12 +30,13 @@ const buildSchema = () => Yup.object().shape({
         data: Yup.string()
           .when('kind', {
             is: (val) => val === 'text',
-            then: Yup.string().min(MIN_TEXT_BLOCK_LENGTH, `Text must be at least ${MIN_TEXT_BLOCK_LENGTH} characters`),
+            then: TextBlockSchema,
             otherwise: Yup.string()
           })
       })
     )
-    .min(MIN_BLOCKS, `Post should have at least ${MIN_BLOCKS} block`)
+    .min(MIN_BLOCKS, `Post should have at least ${pluralize(MIN_BLOCKS, 'block')}.`)
+    .max(MAX_BLOCKS, `Post can have ${pluralize(MAX_BLOCKS, 'block')} max.`)
 });
 
 export default buildSchema
