a6a4a8b4996da7fd0672d9232edb0b96e8ffc032
olehmell <oleh.melnechyk@gmail.com>
2020-05-28 20:18:36 +0300

    Revert postPreview

--- src/components/posts/PostPreview.tsx
+++ src/components/posts/PostPreview.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ViewPostPage from '../posts/ViewPost';
-import { PostWithAllDetails } from '@subsocial/types';
+import { PostWithAllDetails, BlogData } from '@subsocial/types';
 
 type PostPreviewProps = {
   post: PostWithAllDetails
 }
 
 export function PostPreview (props: PostPreviewProps) {
+  const { post, ext, owner, blog = {} as BlogData } = props.post
 
+  return <ViewPostPage postData={post} blog={blog.struct} postExtData={ext} owner={owner} variant='preview' withBlogName />
 }
 
 export default PostPreview
