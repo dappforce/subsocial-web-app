05cd2dd56759f1e7cc6b7ea9a56a50194ec49c86
Alex Siman <aleksandr.siman@gmail.com>
2020-06-25 09:53:08 +0300

    Delete unused code from utils/utils

--- src/components/utils/utils.tsx
+++ src/components/utils/utils.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Pagination as SuiPagination } from 'semantic-ui-react';
 import { Option } from '@polkadot/types';
 import { Icon } from 'antd';
 import moment from 'moment-timezone';
@@ -7,44 +6,6 @@ import BN from 'bn.js';
 import { Profile, SocialAccount } from '@subsocial/types/substrate/interfaces';
 import { ProfileContent } from '@subsocial/types/offchain';
 import { Moment } from '@polkadot/types/interfaces';
-
-type PaginationProps = {
-  currentPage?: number;
-  totalItems: number;
-  itemsPerPage?: number;
-  onPageChange: (activePage?: string | number) => void;
-};
-
-export const Pagination = (p: PaginationProps) => {
-  const { currentPage = 1, itemsPerPage = 20 } = p;
-  const totalPages = Math.floor(p.totalItems / itemsPerPage);
-
-  return totalPages <= 1 ? null : (
-    <SuiPagination
-      firstItem={null}
-      lastItem={null}
-      defaultActivePage={currentPage}
-      totalPages={totalPages}
-      onPageChange={(_event, { activePage }) => p.onPageChange(activePage)}
-    />
-  );
-};
-
-export type UrlHasIdProps = {
-  match: {
-    params: {
-      id: string;
-    };
-  };
-};
-
-export type UrlHasAddressProps = {
-  match: {
-    params: {
-      address: string;
-    };
-  };
-};
 
 type PropsWithSocialAccount = {
   profile?: Profile;
