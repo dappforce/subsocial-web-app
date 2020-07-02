d2c9f4136ffc80595a5a35d348df9a4cb300710b
olehmell <oleh.melnechyk@gmail.com>
2020-07-02 20:36:14 +0300

    Clear hidden button

--- src/components/spaces/HiddenSpaceButton.tsx
+++ src/components/spaces/HiddenSpaceButton.tsx
@@ -14,16 +14,11 @@ export function HiddenSpaceButton (props: HiddenSpaceButtonProps) {
   const { space } = props;
   const hidden = space.hidden.valueOf()
 
-  console.log('new OptionBool(!hidden)', new OptionBool(!hidden), !hidden, hidden)
-  console.log('new OptionBool(true)', new OptionBool(true))
-
   const update = new SpaceUpdate({
     handle: new OptionOptionText(),
     ipfs_hash: new OptionText(),
     hidden: new OptionBool(!hidden) // TODO has no implementation on UI
   });
-
-  console.log('HiddenSpaceButton', update)
 
   const newTxParams = () => {
     return [ space.id, update ];
