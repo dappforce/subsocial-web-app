50dba3c5ae0a34c797165bb54975387f9ef24981
Alex Siman <aleksandr.siman@gmail.com>
2020-07-04 17:59:03 +0300

    Use only new Substrate connection in Layout

--- src/layout/ClientLayout.tsx
+++ src/layout/ClientLayout.tsx
@@ -1,46 +1,26 @@
 import React from 'react';
-
-import settings from '../components/settings';
 import '../components/utils/styles';
-
-import Api from '../components/utils/Api'
 import { SubsocialApiProvider } from '../components/utils/SubsocialApiContext';
-// import Queue from '@subsocial/react-components/Status/Queue';
-import Signer from '@subsocial/react-signer';
 import { MyAccountProvider } from '../components/auth/MyAccountContext';
-// import { Events } from '@subsocial/react-query';
-import { substrateUrl } from '../components/utils/env';
-// import { NotifCounterProvider } from '../components/utils/NotifCounter';
-import { Content } from '../components/main/Content';
+import { Navigation } from './Navigation'
 import SidebarCollapsedProvider from '../components/utils/SideBarCollapsedContext';
-import { AuthProvider } from 'src/components/auth/AuthContext';
-import { SubstrateProvider } from 'src/components/substrate';
-import SubstrateWebConsole from 'src/components/substrate/SubstrateWebConsole';
+import { AuthProvider } from '../components/auth/AuthContext';
+import { SubstrateProvider, SubstrateWebConsole } from '../components/substrate';
 
 const ClientLayout: React.FunctionComponent = ({ children }) => {
-  const url = substrateUrl || settings.apiUrl || undefined;
-
   return (
     <SidebarCollapsedProvider>
       <MyAccountProvider>
-        <Api url={url}> {/* // TODO this should be deleted */}
-          <SubstrateProvider>
-            <SubstrateWebConsole />
-            <SubsocialApiProvider>
-              <AuthProvider>
-                {/* <Events> */}
-                {/* <NotifCounterProvider> */}
-                <Signer>
-                  <Content>
-                    {children}
-                  </Content>
-                </Signer>
-                {/* </NotifCounterProvider> */}
-                {/* </Events> */}
-              </AuthProvider>
-            </SubsocialApiProvider>
-          </SubstrateProvider>
-        </Api>
+        <SubstrateProvider>
+          <SubstrateWebConsole />
+          <SubsocialApiProvider>
+            <AuthProvider>
+              <Navigation>
+                {children}
+              </Navigation>
+            </AuthProvider>
+          </SubsocialApiProvider>
+        </SubstrateProvider>
       </MyAccountProvider>
     </SidebarCollapsedProvider>
   )
