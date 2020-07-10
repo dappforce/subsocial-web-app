e45d6f66912f435c1a78b8d626f773be58bdc77c
olehmell <oleh.melnechyk@gmail.com>
2020-07-10 14:52:53 +0300

    Add Balance components

--- src/components/utils/Balance.tsx
+++ src/components/utils/Balance.tsx
@@ -1,20 +1,46 @@
 import React, { useState } from 'react'
 import { AnyAccountId } from '@subsocial/types';
 import useSubsocialEffect from '../api/useSubsocialEffect'
+import { gtZero } from '.';
+import { newLogger } from '@subsocial/utils';
+
+const log = newLogger('useGetBallance')
 
 const useGetBalance = (address: AnyAccountId) => {
   const [ balance, setBalance ] = useState<string>()
 
   useSubsocialEffect(({ substrate }) => {
-    let unsub: (() => void ) | undefined;
+    let unsub: (() => void) | undefined;
 
     const sub = async () => {
       const api = await substrate.api;
 
       unsub = await api.derive.balances.all(address, (data) => {
         const balance = data.freeBalance
-        gtZero()
+        gtZero(balance) && setBalance(balance.toString())
       });
     }
+
+    sub().catch(err => log.error('Failed load balance %o', err))
+
+    return () => unsub && unsub()
   })
+
+  return balance
+}
+
+type BalanceProps = {
+  address: AnyAccountId,
+  label?: React.ReactNode
+}
+
+export const Balance = ({ address, label }: BalanceProps) => {
+  const balance = useGetBalance(address)
+
+  if (!balance) return null;
+
+  return <span className='DfBalance'>
+    {label}
+    {balance}
+  </span>
 }
