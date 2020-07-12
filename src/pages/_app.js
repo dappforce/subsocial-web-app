4652d586bdeed190a5e713a149866f2a8c628486
Alex Siman <aleksandr.siman@gmail.com>
2020-07-12 21:15:38 +0300

    Move styles up to /src/styles folder

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import App from 'next/app';
-import Head from 'next/head';
+import App from 'next/app'
+import Head from 'next/head'
 import MainPage from '../layout/MainPage'
-import { Provider } from 'react-redux';
-import store from 'src/redux/store';
+import { Provider } from 'react-redux'
+import store from 'src/redux/store'
 
 // TODO remove global import of all AntD CSS, use modular LESS loading instead.
-import 'antd/dist/antd.css';
-import 'antd-mobile/dist/antd-mobile.css';
+import 'antd/dist/antd.css'
+import 'antd-mobile/dist/antd-mobile.css'
 
-import 'src/components/utils/styles/bootstrap-utilities-4.3.1.css';
-import 'src/components/utils/styles/app.scss';
-import 'src/components/utils/styles/components.scss';
-import 'src/components/utils/styles/github-markdown.css';
-import 'src/components/utils/styles/subsocial.scss';
-import 'src/components/utils/styles/utils.scss';
-import 'src/components/utils/styles/subsocial-mobile.scss';
+import 'src/styles/bootstrap-utilities-4.3.1.css'
+import 'src/styles/app.scss'
+import 'src/styles/components.scss'
+import 'src/styles/github-markdown.css'
+import 'src/styles/subsocial.scss'
+import 'src/styles/utils.scss'
+import 'src/styles/subsocial-mobile.scss'
 
-import 'easymde/dist/easymde.min.css';
+import 'easymde/dist/easymde.min.css'
 
 function MyApp (props) {
   const { Component, pageProps } = props
@@ -37,7 +37,7 @@ function MyApp (props) {
 
 MyApp.getInitialProps = async (appContext) => {
   // calls page's `getInitialProps` and fills `appProps.pageProps`
-  const appProps = await App.getInitialProps(appContext);
+  const appProps = await App.getInitialProps(appContext)
 
   return { ...appProps }
 }
