656284606e04df9787e44d5108e0e7c1a9cbf1c8
Alex Siman <aleksandr.siman@gmail.com>
2020-07-12 21:00:59 +0300

    Rename our styles from *.css to *.scss

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,20 @@ import MainPage from '../layout/MainPage'
 import { Provider } from 'react-redux';
 import store from 'src/redux/store';
 
+// TODO remove global import of all AntD CSS, use modular LESS loading instead.
+import 'antd/dist/antd.css';
+import 'antd-mobile/dist/antd-mobile.css';
+
+import 'src/components/utils/styles/bootstrap-utilities-4.3.1.css';
+import 'src/components/utils/styles/app.scss';
+import 'src/components/utils/styles/components.scss';
+import 'src/components/utils/styles/github-markdown.css';
+import 'src/components/utils/styles/subsocial.scss';
+import 'src/components/utils/styles/utils.scss';
+import 'src/components/utils/styles/subsocial-mobile.scss';
+
+import 'easymde/dist/easymde.min.css';
+
 function MyApp (props) {
   const { Component, pageProps } = props
   return (
