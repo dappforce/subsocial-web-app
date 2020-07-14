112db12ba3dec30275f68ec22cb021e5f2f5bd01
olehmell <oleh.melnechyk@gmail.com>
2020-07-14 18:01:36 +0300

    Fix view comment as regularPostPreview

--- src/components/posts/view-post/PostPreview.tsx
+++ src/components/posts/view-post/PostPreview.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { RegularPreview, SharedPreview, isRegularPost, HiddenPostAlert } from '.';
+import { RegularPreview, SharedPreview, HiddenPostAlert } from '.';
 import { PostWithSomeDetails, PostWithAllDetails, SpaceData } from '@subsocial/types';
 import { PostExtension } from '@subsocial/types/substrate/classes';
 import { Segment } from 'src/components/utils/Segment';
+import { isSharedPost } from './helpers';
 export type BarePreviewProps = {
   withActions?: boolean,
   replies?: PostWithAllDetails[],
@@ -16,14 +17,15 @@ export type PreviewProps = BarePreviewProps & {
 
 export function PostPreview (props: PreviewProps) {
   const { postDetails, space: externalSpace, asRegularPost } = props
-  const { space: globalSpace, post: { struct: { extension } } } = postDetails
+  const { space: globalSpace, post: { struct } } = postDetails
+  const { extension } = struct
   const space = externalSpace || globalSpace
 
   if (!space) return null
 
   return <Segment className='DfPostPreview'>
-    <HiddenPostAlert post={postDetails} />
-    {asRegularPost || isRegularPost(extension as PostExtension)
+    <HiddenPostAlert post={struct} space={space} preview />
+    {asRegularPost || !isSharedPost(extension as PostExtension)
       ? <RegularPreview space={externalSpace || space} {...props} />
       : <SharedPreview space={externalSpace || space} {...props} />}
   </Segment>
