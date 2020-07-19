b6df52fd8f615570bbc2c21b4005dfcaa9e61504
Alex Siman <aleksandr.siman@gmail.com>
2020-07-20 02:05:18 +0300

    Support more fields on a new Edit Space form

--- src/components/spaces/EditSpace-ant.tsx
+++ src/components/spaces/EditSpace-ant.tsx
@@ -11,34 +11,43 @@ import { IpfsHash } from '@subsocial/types/substrate/interfaces'
 import { SpaceContent, SpaceData } from '@subsocial/types'
 import { newLogger } from '@subsocial/utils'
 import { useSubsocialApi } from '../utils/SubsocialApiContext'
-import { ValidationProps } from './SpaceValidation'
 import useSubsocialEffect from '../api/useSubsocialEffect'
 import { useMyAddress } from '../auth/MyAccountContext'
-import { DfForm, DfFormButtons } from '../forms'
+import { DfForm, DfFormButtons, minLenError, maxLenError } from '../forms'
 import { Loading } from '../utils'
 import NoData from '../utils/EmptyList'
+import { Codec } from '@polkadot/types/types'
 
 const log = newLogger(EditSpace.name)
+
+const NAME_MIN_LEN = 3
+const NAME_MAX_LEN = 100
 
 type FormValues = Partial<SpaceContent & {
   handle: string
 }>
 
-export type FieldNames = {
-  [s: string]: keyof FormValues
-}
+type FieldName = keyof FormValues
 
-const FieldNames = {
+type FieldNames = Record<FieldName, FieldName>
+
+const FieldNames: FieldNames = {
   handle: 'handle',
   name: 'name',
   desc: 'desc',
   image: 'image',
-  tags: 'tags'
+  tags: 'tags',
+  navTabs: 'navTabs'
 }
 
-type LoadedProps = Partial<SpaceData>
+type LoadedDataProps = Partial<SpaceData>
 
-type OuterProps = ValidationProps & LoadedProps & {
+type ValidationProps = {
+  minHandleLen: number
+  maxHandleLen: number
+}
+
+type OuterProps = ValidationProps & LoadedDataProps & {
   spaceId?: BN
 }
 
@@ -52,13 +61,13 @@ function getInitialValues (props: OuterProps): FormValues {
 
 export function InnerForm (props: OuterProps) {
   const [ form ] = Form.useForm()
-  const { spaceId, struct } = props
+  const { spaceId, struct, minHandleLen, maxHandleLen } = props
   const { ipfs } = useSubsocialApi()
   const [ ipfsHash ] = useState<IpfsHash>()
   const initialValues = getInitialValues(props)
 
   const pinToIpfsAndBuildTxParams = () =>
-    [] // TODO mock
+    [] // TODO impl!
     // getTxParams({
     //   json: { name, desc, image, tags, navTabs },
     //   buildTxParamsCallback: newTxParams,
@@ -83,16 +92,41 @@ export function InnerForm (props: OuterProps) {
   return <>
     <DfForm form={form} initialValues={initialValues}>
       <Form.Item
+        name={FieldNames.name}
+        label='Space name'
+        hasFeedback
+        rules={[
+          { required: true, message: 'Name is required.' },
+          { min: NAME_MIN_LEN, message: minLenError('Name', NAME_MIN_LEN) },
+          { max: NAME_MAX_LEN, message: maxLenError('Name', NAME_MAX_LEN) }
+        ]}
+      >
+        <Input placeholder='Name of your space' />
+      </Form.Item>
+
+      <Form.Item
         name={FieldNames.handle}
         label='URL handle'
         hasFeedback
         rules={[
-          // { required: true },
-          { pattern: /^[A-Za-z0-9_]+$/, message: 'Handle can have only letters (a-z, A-Z), numbers (0-9) and underscores (_)' }
-          // TODO test that handle is unique
+          { pattern: /^[A-Za-z0-9_]+$/, message: 'Handle can have only letters (a-z, A-Z), numbers (0-9) and underscores (_).' },
+          { min: minHandleLen, message: minLenError('Handle', minHandleLen) },
+          { max: maxHandleLen, message: maxLenError('Handle', maxHandleLen) }
+          // TODO test that handle is unique via a call to Substrate
         ]}
       >
-        <Input placeholder='You can use a-z, 0-9 and underscores.' />
+        <Input placeholder='You can use a-z, 0-9 and underscores' />
+      </Form.Item>
+
+      <Form.Item
+        name={FieldNames.image}
+        label='Avatar URL'
+        hasFeedback
+        rules={[
+          { type: 'url', message: 'Should be a valid image URL.' }
+        ]}
+      >
+        <Input placeholder='Image URL' />
       </Form.Item>
 
       <DfFormButtons
@@ -113,16 +147,30 @@ export function InnerForm (props: OuterProps) {
   </>
 }
 
-// TODO load data from minHandleLen, maxHandleLen
+function bnToNum (bn: Codec, _default: number): number {
+  return bn ? (bn as unknown as BN).toNumber() : _default
+}
 
 export function FormInSection (props: OuterProps) {
+  const [ consts, setConsts ] = useState<ValidationProps>()
   const { struct } = props
   const title = struct ? `Edit space` : `New space`
+
+  useSubsocialEffect(({ substrate }) => {
+    const load = async () => {
+      const api = await substrate.api
+      setConsts({
+        minHandleLen: bnToNum(api.consts.spaces.minHandleLen, 5),
+        maxHandleLen: bnToNum(api.consts.spaces.maxHandleLen, 50)
+      })
+    }
+    load()
+  }, [ false ])
 
   return <>
     <HeadMeta title={title} />
     <Section className='EditEntityBox' title={title}>
-      <InnerForm {...props} />
+      <InnerForm {...props} {...consts} />
     </Section>
   </>
 }
@@ -133,7 +181,7 @@ export function EditSpace (props: OuterProps) {
   const idOrHandle = useRouter().query.spaceId as string
   const myAddress = useMyAddress()
   const [ isLoaded, setIsLoaded ] = useState(false)
-  const [ loadedData, setLoadedData ] = useState<LoadedProps>()
+  const [ loadedData, setLoadedData ] = useState<LoadedDataProps>()
 
   useSubsocialEffect(({ subsocial }) => {
     const load = async () => {
