8409fa004b3e902da22b175368552b1bd4382dee
olehmell <oleh.melnechyk@gmail.com>
2020-08-26 17:38:52 +0300

    Create base avatar component

---
+++ src/components/utils/DfAvatar.tsx
@@ -0,0 +1,31 @@
+import React, { CSSProperties } from 'react'
+import { nonEmptyStr } from '@subsocial/utils';
+import { DfBgImg } from 'src/components/utils/DfBgImg';
+import IdentityIcon from 'src/components/utils/IdentityIcon';
+import { AnyAccountId } from '@subsocial/types/substrate';
+import { DEFAULT_AVATAR_SIZE } from 'src/config/Size.config';
+
+export type BaseAvatarProps = {
+  size?: number,
+  style?: CSSProperties,
+  avatar?: string
+  address: AnyAccountId,
+}
+
+const SUB_SIZE = 2;
+
+export const BaseAvatar = ({ size = DEFAULT_AVATAR_SIZE, avatar, style, address }: BaseAvatarProps) => {
+  const icon = nonEmptyStr(avatar)
+    ? <DfBgImg size={size} src={avatar} className='DfAvatar space ui--IdentityIcon' style={style} rounded />
+    : <IdentityIcon
+      style={style}
+      size={size - SUB_SIZE}
+      value={address}
+    />;
+
+  if (!icon) return null
+
+  return icon
+}
+
+export default BaseAvatar
