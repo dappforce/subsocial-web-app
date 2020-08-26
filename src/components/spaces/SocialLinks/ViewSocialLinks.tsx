abb39d528212015b6c76cac37f2892b412991cfd
olehmell <oleh.melnechyk@gmail.com>
2020-08-26 17:39:31 +0300

    Improve social links(change text to img)

--- src/components/spaces/SocialLinks/ViewSocialLinks.tsx
+++ src/components/spaces/SocialLinks/ViewSocialLinks.tsx
@@ -1,54 +1,20 @@
 import React from 'react'
-import {
-  FacebookOutlined,
-  TwitterOutlined,
-  MediumOutlined,
-  LinkedinOutlined,
-  GithubOutlined,
-  InstagramOutlined,
-  GlobalOutlined,
-  MailOutlined
-} from '@ant-design/icons'
 import { NamedLinks } from '@subsocial/types'
-
-const socialLinksRegExp: Record<string, RegExp> = {
-  Facebook: /(?:https?:\/\/)?(?:www\.)?(?:facebook|fb|m\.facebook)\.(?:com|me)/i,
-  Twitter: /(?:https?:\/\/)?(?:www\.)?(?:twitter)\.(?:com)/i,
-  Medium: /(?:https?:\/\/)?(?:www\.)?(?:medium)\.(?:com)/i,
-  LinkedIn: /(?:https?:\/\/)?(?:www\.)?(?:linkedin)\.(?:com)/i,
-  Github: /(?:https?:\/\/)?(?:www\.)?(?:github)\.(?:com)/i,
-  Instagram: /(?:https?:\/\/)?(?:www\.)?(?:instagram)\.(?:com)/i,
-  Telegram: /(?:https?:\/\/)?(?:www\.)?(?:telegram|t|)\.(?:com|me)/i
-}
-
-const getSocialLinkType = (link: string) => {
-  for (const key in socialLinksRegExp) {
-    if (socialLinksRegExp[key].test(link)) return key
-  }
-
-  return undefined
-}
+import { getSocialLinkType, getLinksAttr } from './utils'
+import { MailOutlined } from '@ant-design/icons'
 
 type SocialLinkProps = {
   link: string
 }
 
-const getLinksAttr = (link: string) => {
+const attrByLink = (link: string) => {
   const title = getSocialLinkType(link)
 
-  switch (title) {
-    case 'Facebook': return { title, icon: <FacebookOutlined /> }
-    case 'Twitter': return { title, icon: <TwitterOutlined /> }
-    case 'Medium': return { title, icon: <MediumOutlined /> }
-    case 'LinkedIn': return { title, icon: <LinkedinOutlined /> }
-    case 'Github': return { title, icon: <GithubOutlined /> }
-    case 'Instagram': return { title, icon: <InstagramOutlined /> }
-    default: return { title, icon: <GlobalOutlined /> }
-  }
+  return getLinksAttr(title)
 }
 
 const SocialLink = ({ link }: SocialLinkProps) => {
-  const { title, icon } = getLinksAttr(link)
+  const { title, icon } = attrByLink(link)
   return <a href={link} title={title} className='DfBlackLink mr-3'>
     {icon}
   </a>
@@ -67,7 +33,6 @@ type ContactInfoProps = SocialLinksProps & {
 }
 
 export const ContactInfo = ({ links, email }: ContactInfoProps) => {
-  console.log(links)
   if (!links && !email) return null
 
   return <div>
