36f44ec2f39774023448f56953dd6d40915f0a58
olehmell <oleh.melnechyk@gmail.com>
2020-09-03 15:37:44 +0300

    Add space between tokens and currency

--- src/components/profiles/address-views/utils/Balance.tsx
+++ src/components/profiles/address-views/utils/Balance.tsx
@@ -21,7 +21,7 @@ function format (value: Compact<any> | BN | string, currency: string, withSi?: b
     return formatBalance(value);
   }
 
-  return <>{prefix}{!isShort && (<>.<span className='ui--FormatBalance-postfix'>{`000${postfix || ''}`.slice(-3)}</span></>)} {currency}</>;
+  return <>{prefix}{!isShort && (<>.<span className='ui--FormatBalance-postfix'>{`000${postfix || ''}`.slice(-3)}</span></>)}&nbsp;{currency}</>;
 }
 
 const useGetBalance = (address: AnyAccountId) => {
