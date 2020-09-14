078871fc5ef95910880ccbe4baf6a2c31744e1a3
olehmell <oleh.melnechyk@gmail.com>
2020-09-14 21:44:56 +0300

    Improve resolveIpfsUrl function

--- src/ipfs/index.ts
+++ src/ipfs/index.ts
@@ -1,11 +1,11 @@
 import { ipfsNodeUrl } from 'src/components/utils/env';
 import CID from 'cids'
 
-const getPath = 'api/v0/cat?arg='
+const getPath = (cid: string) => `api/v0/cat?arg=${cid}`
 
 export const resolveIpfsUrl = (cid: string) => {
   try {
-    return CID.isCID(new CID(cid)) ? `${ipfsNodeUrl}/${getPath}/${cid}` : cid
+    return CID.isCID(new CID(cid)) ? `${ipfsNodeUrl}/${getPath(cid)}` : cid
   } catch (err) {
     return cid
   }
