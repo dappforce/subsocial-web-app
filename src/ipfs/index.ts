fb5b4da6e9bc83f1f0d72ecf1df0c738beeb7495
olehmell <oleh.melnechyk@gmail.com>
2020-09-14 13:48:17 +0300

    Fix resolve ipfs img

--- src/ipfs/index.ts
+++ src/ipfs/index.ts
@@ -1,9 +1,11 @@
 import { ipfsNodeUrl } from 'src/components/utils/env';
 import CID from 'cids'
 
+const getPath = 'api/v0/cat?arg='
+
 export const resolveIpfsUrl = (cid: string) => {
   try {
-    return CID.isCID(new CID(cid)) ? `${ipfsNodeUrl}/ipfs/${cid}` : cid
+    return CID.isCID(new CID(cid)) ? `${ipfsNodeUrl}/${getPath}/${cid}` : cid
   } catch (err) {
     return cid
   }
