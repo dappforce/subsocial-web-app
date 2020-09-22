cc0d53c211621f379871111f0db6bd5be57a3fba
olehmell <oleh.melnechyk@gmail.com>
2020-09-22 14:42:03 +0300

    Rename in storybook

--- src/stories/Notifications.stories.tsx
+++ src/stories/Notifications.stories.tsx
@@ -3,11 +3,11 @@ import { Notification } from '../components/activity/Notification';
 import { mockProfileDataAlice } from './mocks/SocialProfileMocks';
 import { mockAccountAlice } from './mocks/AccountMocks'
 import { mockSpaceDataAlice } from './mocks/SpaceMocks'
-import { ViewSpacePage } from '../components/spaces/ViewSpace'
+import { ViewSpace } from '../components/spaces/ViewSpace'
 
 export default {
   title: 'Activity | Notifications'
 }
 
 export const _MyNotifications = () =>
-  <Notification address={mockAccountAlice.toString()} owner={mockProfileDataAlice} details={new Date().toLocaleString()} textActivity={<>and 1 people use here notification <ViewSpacePage spaceData={mockSpaceDataAlice} nameOnly withLink /></>}/>
+  <Notification address={mockAccountAlice.toString()} owner={mockProfileDataAlice} details={new Date().toLocaleString()} textActivity={<>and 1 people use here notification <ViewSpace spaceData={mockSpaceDataAlice} nameOnly withLink /></>}/>
