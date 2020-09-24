3f824101ad0517dd6eec56d98b491fa40e9ceec8
Alex Siman <aleksandr.siman@gmail.com>
2020-09-24 23:02:21 +0300

    Render summary immediately in SummarizeMd

--- src/components/utils/md/SummarizeMd.tsx
+++ src/components/utils/md/SummarizeMd.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { isEmptyStr } from '@subsocial/utils'
 import { mdToText } from './mdToText'
+import mdToTextSync from 'markdown-to-txt'
 import { summarize } from '../text'
 
 type Props = {
@@ -12,15 +13,25 @@ type Props = {
 export const SummarizeMd = ({ md, limit, more }: Props) => {
   if (isEmptyStr(md)) return null
 
-  const [ summary, setSummary ] = useState<string>()
+  const getSummary = (s?: string): string => summarize(s, limit)
+
+  // Here we get the first version of markdown to text.
+  // It is not perfect but sync. and this allows us to return a result immediately
+  // - this is important for SEO via server-side rendering.
+  const syncSummary = getSummary(mdToTextSync(md))
+
+  const [ summary, setSummary ] = useState<string>(syncSummary)
   const [ showMore, setShowMore ] = useState<boolean>(false)
 
   useEffect(() => {
     let isSubscribe = true
 
     const process = async () => {
+      // Here we get a async version of summary by using another async function
+      // of markdown to text. Its output is better, but async.
+      // That's why we call it in useEffect.
       const text = (await mdToText(md))?.trim()
-      const summary = summarize(text, limit)
+      const summary = getSummary(text)
       isSubscribe && setSummary(summary)
       if (isSubscribe && text && text.length > summary.length) {
         setShowMore(true)
