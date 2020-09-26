e34ff2d2c120df2282ed9667950f4a96927bf44e
Alex Siman <aleksandr.siman@gmail.com>
2020-09-26 03:13:33 +0300

    Fix bug: allow empty tags on forms

--- src/components/utils/content/index.ts
+++ src/components/utils/content/index.ts
@@ -1,10 +1,15 @@
 import { SpaceContent, PostContent } from "@subsocial/types"
 import { nonEmptyStr } from "@subsocial/utils"
 
-export const resolveContent = <T extends SpaceContent | PostContent>(content: T): T => {
+type Content = SpaceContent | PostContent
+
+export const resolveContent = <T extends Content>(content: T): T => {
   const { tags, ...otherContent } = content
 
   return { tags: resolveTags(tags), ...otherContent } as T
 }
 
-export const resolveTags = (tags: string[]) => tags.filter(nonEmptyStr).map(x => x.trim())
+export const resolveTags = (tags: string[] = []) =>
+  tags
+    .filter(nonEmptyStr)
+    .map(x => x.trim())
