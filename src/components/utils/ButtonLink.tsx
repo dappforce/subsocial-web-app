e69df834c44293e32a9db40db845b722af297537
Vladyslav Proshchavaiev <f3joule@gmail.com>
2020-09-28 16:17:14 +0300

    Fix "Proceed to faucet on Telegram" button
    Now button opens a new tab in a browser

--- src/components/utils/ButtonLink.tsx
+++ src/components/utils/ButtonLink.tsx
@@ -4,13 +4,14 @@ import Link from 'next/link'
 
 type ButtonLinkProps = ButtonProps & {
   href: string,
-  as?: string
+  as?: string,
+  target?: string
 }
 
-export const ButtonLink = ({ as, href, children, ...buttonProps }: ButtonLinkProps) =>
+export const ButtonLink = ({ as, href, target, children, ...buttonProps }: ButtonLinkProps) =>
   <Button {...buttonProps}>
     <Link href={href} as={as}>
-      <a>
+      <a target={target}>
         {children}
       </a>
     </Link>
