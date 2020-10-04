6dac3f53c23afda5471fa8331b83c8f9fd483f66
Alex Siman <aleksandr.siman@gmail.com>
2020-10-04 15:14:04 +0300

    Add a button "See all spaces" after latest spaces on home page

--- src/components/main/LatestSpaces.tsx
+++ src/components/main/LatestSpaces.tsx
@@ -4,6 +4,7 @@ import { ViewSpace } from '../spaces/ViewSpace';
 import { SpaceData } from '@subsocial/types/dto';
 import { CreateSpaceButton, AllSpacesLink } from '../spaces/helpers';
 import DataList from '../lists/DataList';
+import { ButtonLink } from 'src/components/utils/ButtonLink';
 
 type Props = {
   spacesData: SpaceData[]
@@ -13,22 +14,25 @@ export const LatestSpaces = (props: Props) => {
   const { spacesData = [] } = props
   const spaces = spacesData.filter((x) => typeof x.struct !== 'undefined')
 
-  return <DataList
-    title={<span className='d-flex justify-content-between align-items-end w-100'>
-      {'Latest spaces'}
-      <AllSpacesLink />
-    </span>}
-    dataSource={spaces}
-    noDataDesc='No spaces created yet'
-    noDataExt={<CreateSpaceButton />}
-    renderItem={(item) =>
-      <ViewSpace
-        {...props}
-        key={(item.struct as Space).id.toString()}
-        spaceData={item}
-        withFollowButton
-        preview
-      />
-    }
-  />
+  return <>
+    <DataList
+      title={<span className='d-flex justify-content-between align-items-end w-100'>
+        {'Latest spaces'}
+        <AllSpacesLink />
+      </span>}
+      dataSource={spaces}
+      noDataDesc='No spaces created yet'
+      noDataExt={<CreateSpaceButton />}
+      renderItem={(item) =>
+        <ViewSpace
+          {...props}
+          key={(item.struct as Space).id.toString()}
+          spaceData={item}
+          withFollowButton
+          preview
+        />
+      }
+    />
+    <ButtonLink block href='/spaces/all' as='/spaces/all' className='mb-2'>See all spaces</ButtonLink>
+  </>
 }
