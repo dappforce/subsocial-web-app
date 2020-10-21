11cf3b455fa2c0be83f077988cc2443168337be4
olehmell <oleh.melnechyk@gmail.com>
2020-10-21 19:14:38 +0300

    Change size

--- src/config/ListData.config.ts
+++ src/config/ListData.config.ts
@@ -2,4 +2,4 @@ export const DEFAULT_FIRST_PAGE = 1;
 export const DEFAULT_PAGE_SIZE = 10;
 export const MAX_PAGE_SIZE = 100;
 export const PAGE_SIZE_OPTIONS = [ 10, 20, 50, 100 ];
-export const INFINITE_SCROLL_PAGE_SIZE = 50;
+export const INFINITE_SCROLL_PAGE_SIZE = 20;
