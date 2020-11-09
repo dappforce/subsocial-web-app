b593dad6a666135e44dca72aabda2e4f80baf498
olehmell <oleh.melnechyk@gmail.com>
2020-11-09 22:21:37 +0200

    Revert eslint format

--- src/components/search/SearchResults.tsx
+++ src/components/search/SearchResults.tsx
@@ -1,157 +1,123 @@
-import React, { useState, useCallback } from "react";
-import { ViewSpace } from "../spaces/ViewSpace";
-import { Segment } from "src/components/utils/Segment";
-import { Tabs } from "antd";
-import {
-  ElasticIndex,
-  ElasticIndexTypes
-} from "@subsocial/types/offchain/search";
-import { useRouter } from "next/router";
-import Section from "../utils/Section";
-import { ProfilePreviewWithOwner } from "../profiles/address-views";
-import { DataListOptProps } from "../lists/DataList";
-import { queryElasticSearch } from "src/components/utils/OffchainUtils";
-import {
-  InfiniteListByData,
-  InnerLoadMoreFn,
-  RenderItemFn
-} from "../lists/InfiniteList";
-import PostPreview from "../posts/view-post/PostPreview";
-import {
-  AnySubsocialData,
-  PostWithAllDetails,
-  ProfileData,
-  SpaceData
-} from "@subsocial/types";
+import React, { useState, useCallback } from 'react'
+import { ViewSpace } from '../spaces/ViewSpace'
+import { Segment } from 'src/components/utils/Segment'
+import { Tabs } from 'antd'
+import { ElasticIndex, ElasticIndexTypes } from '@subsocial/types/offchain/search'
+import { useRouter } from 'next/router'
+import Section from '../utils/Section'
+import { ProfilePreviewWithOwner } from '../profiles/address-views'
+import { DataListOptProps } from '../lists/DataList'
+import { queryElasticSearch } from 'src/components/utils/OffchainUtils'
+import { InfiniteListByData, InnerLoadMoreFn, RenderItemFn } from '../lists/InfiniteList'
+import PostPreview from '../posts/view-post/PostPreview'
+import { AnySubsocialData, PostWithAllDetails, ProfileData, SpaceData } from '@subsocial/types'
 
-const { TabPane } = Tabs;
+const { TabPane } = Tabs
 
 type DataResults = {
-  index: string;
-  id: string;
-  data: (AnySubsocialData | PostWithAllDetails)[];
-};
+  index: string
+  id: string
+  data: (AnySubsocialData | PostWithAllDetails)[]
+}
 
-const AllTabKey = "all";
+const AllTabKey = 'all'
 
 const panes = [
   {
     key: AllTabKey,
-    title: "All"
+    title: 'All'
   },
   {
-    key: "spaces",
-    title: "Spaces"
+    key: 'spaces',
+    title: 'Spaces'
   },
   {
-    key: "posts",
-    title: "Posts"
+    key: 'posts',
+    title: 'Posts'
   },
   {
-    key: "profiles",
-    title: "Profiles"
+    key: 'profiles',
+    title: 'Profiles'
   }
-];
+]
 
 const resultToPreview = ({ data, index, id }: DataResults, i: number) => {
-  const unknownData = data as unknown;
+  const unknownData = data as unknown
   switch (index) {
     case ElasticIndex.spaces:
-      return (
-        <ViewSpace
-          key={`${id}-${i}`}
-          spaceData={unknownData as SpaceData}
-          preview
-          withFollowButton
-        />
-      );
+      return <ViewSpace key={`${id}-${i}`} spaceData={unknownData as SpaceData} preview withFollowButton />
     case ElasticIndex.posts: {
-      const postData = unknownData as PostWithAllDetails;
-      return (
-        <PostPreview
-          key={postData.post.struct.id.toString()}
-          postDetails={postData}
-          withActions
-        />
-      );
+      const postData = unknownData as PostWithAllDetails
+      return <PostPreview key={postData.post.struct.id.toString()} postDetails={postData} withActions />
     }
     case ElasticIndex.profiles:
       return (
         <Segment>
-          <ProfilePreviewWithOwner
-            key={`${id}-${i}`}
-            address={id}
-            owner={unknownData as ProfileData}
-          />
+          <ProfilePreviewWithOwner key={`${id}-${i}`} address={id} owner={unknownData as ProfileData} />
         </Segment>
-      );
+      )
     default:
-      return <></>;
+      return <></>
   }
-};
+}
 
 type InnerSearchResultListProps<T> = DataListOptProps & {
-  loadingLabel?: string;
-  renderItem: RenderItemFn<T>;
-};
+  loadingLabel?: string
+  renderItem: RenderItemFn<T>
+}
 
-const InnerSearchResultList = <T extends DataResults>(
-  props: InnerSearchResultListProps<T>
-) => {
-  const router = useRouter();
+const InnerSearchResultList = <T extends DataResults>(props: InnerSearchResultListProps<T>) => {
+  const router = useRouter()
 
-  const getReqParam = (param: "tab" | "q" | "tags") => {
-    return router.query[param];
-  };
+  const getReqParam = (param: 'tab' | 'q' | 'tags') => {
+    return router.query[param]
+  }
 
   const querySearch: InnerLoadMoreFn<T> = async (page, size) => {
-    const tab = getReqParam("tab") as ElasticIndexTypes[];
-    const query = getReqParam("q") as string;
-    const tags = getReqParam("tags") as string[];
-    const offset = (page - 1) * size;
+    const tab = getReqParam('tab') as ElasticIndexTypes[]
+    const query = getReqParam('q') as string
+    const tags = getReqParam('tags') as string[]
+    const offset = (page - 1) * size
 
     const res = await queryElasticSearch({
       indexes: tab || AllTabKey,
       q: query,
       tags,
       offset,
-      limit: size
-    });
+      limit: size,
+    })
 
-    return res;
-  };
+    return res
+  }
 
-  const List = useCallback(
-    () => <InfiniteListByData {...props} loadMore={querySearch} />,
+  const List = useCallback(() =>
+    <InfiniteListByData {...props} loadMore={querySearch} />,
     [ router.asPath ]
-  );
+  )
 
-  return <List />;
-};
+  return <List />
+}
 
 const AllResultsList = () => (
-  <InnerSearchResultList
-    loadingLabel={"Loading search results..."}
-    renderItem={resultToPreview}
-  />
-);
+  <InnerSearchResultList loadingLabel={'Loading search results...'} renderItem={resultToPreview} />
+)
 
 const ResultsTabs = () => {
-  const router = useRouter();
+  const router = useRouter()
 
   const getTabIndexFromUrl = (): number => {
-    const tabFromUrl = router.query.tab;
-    const tabIndex = panes.findIndex(pane => pane.key === tabFromUrl);
-    return tabIndex < 0 ? 0 : tabIndex;
-  };
+    const tabFromUrl = router.query.tab
+    const tabIndex = panes.findIndex(pane => pane.key === tabFromUrl)
+    return tabIndex < 0 ? 0 : tabIndex
+  }
 
-  const initialTabIndex = getTabIndexFromUrl();
-  const initialTabKey = panes[initialTabIndex].key;
+  const initialTabIndex = getTabIndexFromUrl()
+  const initialTabKey = panes[initialTabIndex].key
 
-  const [ activeTabKey, setActiveTabKey ] = useState(initialTabKey);
+  const [activeTabKey, setActiveTabKey] = useState(initialTabKey)
 
   const handleTabChange = (key: string) => {
-    setActiveTabKey(key);
+    setActiveTabKey(key)
 
     const newPath = {
       pathname: router.pathname,
@@ -159,10 +125,10 @@ const ResultsTabs = () => {
         ...router.query,
         tab: key
       }
-    };
+    }
 
-    router.push(newPath, newPath);
-  };
+    router.push(newPath, newPath)
+  }
 
   return (
     <Tabs onChange={handleTabChange} activeKey={activeTabKey.toString()}>
@@ -172,15 +138,15 @@ const ResultsTabs = () => {
         </TabPane>
       ))}
     </Tabs>
-  );
-};
+  )
+}
 
 const SearchResults = () => {
   return (
     <Section>
       <ResultsTabs />
     </Section>
-  );
-};
+  )
+}
 
-export default SearchResults;
+export default SearchResults
