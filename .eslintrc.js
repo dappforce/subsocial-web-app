aed22fac7aba8dd295c8b1e6a6e9eeb8dc36f367
olehmell <oleh.melnechyk@gmail.com>
2020-11-11 10:51:32 +0200

    Push new roles for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
-const base = require('@subsocial/config/eslintrc');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const base = require('@subsocial/config/eslintrc')
 
 // add override for any (a metric ton of them, initial conversion)
 module.exports = {
-  ...base
-};
+  ...base,
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'semi': [ 'warn', 'never' ],
+    'react/prop-types': 'off',
+    'quotes': [ 'warn', 'single' ],
+    'array-bracket-spacing' : [ 'warn', 'always' ],
+    'no-multi-spaces': 'error',
+    'space-before-function-paren': [ 'warn', 'always' ]
+  }
+}
