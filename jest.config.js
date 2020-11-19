d503b49456ec34537c991daf257fa78925570353
olehmell <oleh.melnechyk@gmail.com>
2020-11-19 17:57:55 +0200

    Add jest config

--- jest.config.js
+++ jest.config.js
@@ -4,13 +4,10 @@ module.exports = {
     '!**/*.d.ts',
     '!**/node_modules/**',
   ],
-  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  setupFilesAfterEnv: [ '<rootDir>/setupTests.js' ],
+  testPathIgnorePatterns: [ '/node_modules/', '/.next/' ],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
-  },
-  moduleNameMapper: {
-    'src(.*)$': '<rootDir>/src$1'
   },
   transformIgnorePatterns: [
     '/node_modules/',
