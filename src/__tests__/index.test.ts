da0e2469aab6bebd14f34899c76ae9b5ff264211
olehmell <oleh.melnechyk@gmail.com>
2020-11-19 17:58:02 +0200

    Add test fullUrl

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,9 +1,24 @@
-import { fullUrl } from '../components/urls/helpers'
+import { fullUrl } from '../components/urls/local'
 import { appBaseUrl } from '../components/utils/env'
 
-test('Url', () => {
-  const relativePath = '/spaces/all'
+test('Relative path with "/"', async () => {
+  const relativePath = '/test/test'
   const url = fullUrl(relativePath)
 
   expect(url).toBe(`${appBaseUrl}${relativePath}`)
+})
+
+test('Relative path without "/"', () => {
+  let relativePath = 'test/test'
+  const url = fullUrl(relativePath)
+  relativePath = '/' + relativePath
+
+  expect(url).toBe(`${appBaseUrl}${relativePath}`)
+})
+
+test('Absolute path', () => {
+  const absolutePath = `${appBaseUrl}/test/test`
+  const url = fullUrl(absolutePath)
+
+  expect(url).toBe(absolutePath)
 })
