4bd78597ec5f816becab5c8f682a1bc6ab701537
Alex Siman <aleksandr.siman@gmail.com>
2020-11-21 20:18:22 +0200

    Add env var: SEO_SITEMAP_PAGE_SIZE

--- src/components/utils/env.ts
+++ src/components/utils/env.ts
@@ -1,6 +1,7 @@
 import BN from 'bn.js'
 import { } from '@subsocial/api/utils/types'
 import dayjs from 'dayjs'
+import { DEFAULT_PAGE_SIZE } from 'src/config/ListData.config'
 
 function getEnv (varName: string): string | undefined {
   const { env } = typeof window === 'undefined' ? process : window.process
@@ -35,11 +36,15 @@ export const isProdMode = nodeEnv === 'production'
 export const isDevMode = !isProdMode
 
 export const appName = getEnv('APP_NAME') || 'Subsocial'
+export const appBaseUrl = getEnv('APP_BASE_URL') || ''
 
 export const offchainUrl = getEnv('OFFCHAIN_URL') || 'http://localhost:3001'
 export const offchainWs = getEnv('OFFCHAIN_WS') || 'http://localhost:3011'
 export const ipfsNodeUrl = getEnv('IPFS_URL') || 'http://localhost:8080'
 export const substrateUrl = getEnv('SUBSTRATE_URL') || 'ws://127.0.0.1:9944'
+
+export const landingPageUrl = 'https://subsocial.network'
+export const advancedUrl = `https://polkadot.js.org/apps/?rpc=${substrateUrl}`
 export const kusamaUrl = 'wss://kusama-rpc.polkadot.io'
 
 // UI settings
@@ -49,14 +54,12 @@ export const uiShowFeed = getEnvAsBool('UI_SHOW_FEED')
 export const uiShowNotifications = getEnvAsBool('UI_SHOW_NOTIFICATIONS')
 export const uiShowActivity = getEnvAsBool('UI_SHOW_ACTIVITY')
 
-export const dagHttpMethod = getEnv('DAG_HTTP_METHOD')?.toLowerCase() || 'post'
+// SEO settings
+export const seoSitemapLastmod = getEnvAsDate('SEO_SITEMAP_LASTMOD')
+export const seoSitemapPageSize = getEnvAsNumber('SEO_SITEMAP_PAGE_SIZE') || DEFAULT_PAGE_SIZE
 
-export const advancedUrl = `https://polkadot.js.org/apps/?rpc=${substrateUrl}`
-export const landingPageUrl = 'https://subsocial.network'
-
-export const appBaseUrl = getEnv('APP_BASE_URL') || ''
-
-export const seoOverwriteLastUpdate = getEnvAsDate('SEO_OVERWRITE_LAST_UPDATE')
-
+// Reserved spaces
 export const lastReservedSpaceId = getEnvAsNumber('LAST_RESERVED_SPACE_ID') || 0
 export const claimedSpaceIds = getEnvAsArray('CLAIMED_SPACE_IDS').map(x => new BN(x))
+
+export const dagHttpMethod = getEnv('DAG_HTTP_METHOD')?.toLowerCase() || 'post'
