b1e22c1ae36ebe638768a4da354e98ff54bd4efd
Alex Siman <aleksandr.siman@gmail.com>
2020-11-24 12:55:47 +0200

    New Post button: Go to new post form or show the space selector modal

--- src/components/posts/NewPostButtonInTopMenu.tsx
+++ src/components/posts/NewPostButtonInTopMenu.tsx
@@ -6,7 +6,6 @@ import { newLogger } from '@subsocial/utils'
 import React, { useState } from 'react'
 import { Button, Modal } from 'antd'
 import { useMyAddress } from '../auth/MyAccountContext'
-import { address } from 'faker'
 import useSubsocialEffect from '../api/useSubsocialEffect'
 import { SpaceData } from '@subsocial/types'
 import { findSpaceIdsThatCanSuggestIfSudo } from 'src/utils'
@@ -17,7 +16,7 @@ import { Loading } from '../utils'
 import { NoData } from '../utils/EmptyList'
 import { goToNewPostPage } from '../urls/goToPage'
 
-const log = newLogger('NewPostButtonInTopMenu')
+const log = newLogger('New Post Button In Top Menu')
 
 export const NewPostButtonInTopMenu = () => {
   const myAddress = useMyAddress()
@@ -27,7 +26,7 @@ export const NewPostButtonInTopMenu = () => {
 
   // TODO fix copy-paste, see OnlySudo
   useSubsocialEffect(({ substrate }) => {
-    if (!open) return
+    // if (!open) return
 
     const load = async () => {
       const api = await substrate.api
@@ -36,36 +35,45 @@ export const NewPostButtonInTopMenu = () => {
     }
 
     load()
-  }, [ open, sudo?.toString() ])
+  }, [ sudo?.toString() ])
 
   // TODO fix copy-paste, see AccountSpaces
   useSubsocialEffect(({ subsocial, substrate }) => {
-    if (!open || !sudo || !myAddress) return
+    if (/* !open || */ !sudo || !myAddress) return
 
     const loadSpaces = async () => {
       const mySpaceIds = await substrate.spaceIdsByOwner(myAddress)
       const withoutReservedSpaceIds = findSpaceIdsThatCanSuggestIfSudo(sudo, myAddress, mySpaceIds)
       const spacesData = await subsocial.findPublicSpaces(withoutReservedSpaceIds)
-      
-      if (spacesData.length === 1) {
-        // Redirect to the post edit form if a user has only one space
-        const [ space ] = spacesData
-        closeModal()
-        goToNewPostPage(space.struct)
-      } else {
-        setSpacesData(spacesData)
-      }
+      setSpacesData(spacesData)
     }
 
     loadSpaces().catch((err) =>
-      log.error('Failed to load spaces by account', address, err)
+      log.error('Failed to load spaces by account', myAddress, err)
     )
-  }, [ open, sudo?.toString(), address.toString() ])
+  }, [ sudo?.toString(), myAddress?.toString() ])
+
+  console.table({
+    sudo: sudo?.toString(),
+    myAddress: myAddress?.toString()
+  })
 
   // TODO show "Create Space" in top menu if user has not spaces
   // or at least don't show "New Post" button if a user has no spaces
 
   const closeModal = () => setOpen(false)
+
+  /** Go to new post form or show the space selector modal. */
+  const onNewPostClick = () => {
+    if (spacesData && spacesData.length === 1) {
+      // Redirect to the post edit form if a user has only one space
+      closeModal()
+      goToNewPostPage(spacesData[0].struct)
+    } else {
+      // Show space selector modal
+      setOpen(true)
+    }
+  }
 
   const ListOfSpaces = () => {
     if (!spacesData) {
@@ -90,7 +98,7 @@ export const NewPostButtonInTopMenu = () => {
   }
 
   return <>
-    <Button icon={<PlusOutlined />} onClick={() => setOpen(true)}>New post</Button>
+    <Button icon={<PlusOutlined />} onClick={onNewPostClick}>New post</Button>
     <Modal
       onCancel={closeModal}
       visible={open}
