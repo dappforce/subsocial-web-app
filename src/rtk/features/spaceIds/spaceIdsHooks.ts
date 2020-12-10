cd3c8b6bb8ed8f0ec16420159385ed5ddbb4e237
olehmell <oleh.melnechyk@gmail.com>
2020-12-10 21:32:58 +0200

    Add hooks for spaceIds slice

--- src/rtk/features/spaceIds/spaceIdsHooks.ts
+++ src/rtk/features/spaceIds/spaceIdsHooks.ts
@@ -1,5 +1,8 @@
+import { useRouter } from 'next/router'
+import { getPageOfIds } from 'src/components/utils/getIds'
 import { useFetchOneEntity } from 'src/rtk/app/hooksCommon'
-import { AccountId } from 'src/types'
+import { AccountId, SpaceId } from 'src/types'
+import { useFetchSpaces } from '../spaces/spacesHooks'
 import { fetchSpaceIdsFollowedByAccount, _selectSpaceIdsFollowedByAccount } from './followedSpaceIdsSlice'
 import { fetchSpaceIdsOwnedByAccount, _selectSpaceIdsOwnedByAccount } from './ownedSpaceIdsSlice'
 
@@ -9,4 +12,43 @@ export const useFetchOwnedSpaceIds = (owner: AccountId) => {
 
 export const useFetchFollowedSpaceIds = (follower: AccountId) => {
   return useFetchOneEntity(_selectSpaceIdsFollowedByAccount, fetchSpaceIdsFollowedByAccount, { id: follower })
+}
+
+const useFetchSpacesOfPage = (spaceIds: SpaceId[] = []) => {
+  const { query } = useRouter()
+
+  const ids = getPageOfIds(spaceIds, query)
+  
+  const { entities, error, loading } = useFetchSpaces({ ids })
+
+  return { 
+    spaces: entities,
+    spaceIds,
+    loading,
+    error,
+  }
+}
+
+export const useFetchOwnedSpaces = (owner: AccountId) => {
+  const { entity, loading: l1, error: err1 } = useFetchOwnedSpaceIds(owner)
+  const { spaceIds, spaces, loading: l2, error: err2 } = useFetchSpacesOfPage(entity?.mySpaceIds)
+
+  return { 
+    spaces,
+    spaceIds,
+    loading: l2 || l1,
+    error: err2 || err1,
+  }
+}
+
+export const useFetchFollowedSpaces = (owner: AccountId) => {
+  const { entity, loading: l1, error: err1 } = useFetchFollowedSpaceIds(owner)
+  const { spaceIds, spaces, error: l2, loading: err2 } = useFetchSpacesOfPage(entity?.followedSpaceIds)
+
+  return { 
+    spaces,
+    spaceIds,
+    loading: l2 || l1,
+    error: err2 || err1
+  }
 }
