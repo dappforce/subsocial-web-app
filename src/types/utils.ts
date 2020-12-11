0a0ac66fa8e57732e045edb526624aba5b9e4c65
olehmell <oleh.melnechyk@gmail.com>
2020-12-11 16:55:19 +0200

    small fix types for bnToId

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -42,11 +42,11 @@ export function idToPostId (id: AnyId): PostId {
   return idToBn(id) as PostId
 }
 
-export function bnToId (bnId: BN): EntityId {
+export function bnToId (bnId: AnyId): EntityId {
   return bnId.toString()
 }
 
-export function bnsToIds (bnIds: BN[]): EntityId[] {
+export function bnsToIds (bnIds: AnyId[]): EntityId[] {
   return bnIds.map(bnToId)
 }
 
