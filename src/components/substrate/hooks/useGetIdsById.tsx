151a85a50e0582f6507cb02c4d2384a8fa48625b
olehmell <oleh.melnechyk@gmail.com>
2020-12-15 18:08:10 +0200

    Add get substrate ids hooks

---
+++ src/components/substrate/hooks/useGetIdsById.tsx
@@ -0,0 +1,37 @@
+import AccountId from '@polkadot/types/ethereum/AccountId'
+import { SubstrateId } from '@subsocial/types'
+import { useState } from 'react'
+import useSubsocialEffect from 'src/components/api/useSubsocialEffect'
+import { FetchManyResult } from 'src/rtk/app/hooksCommon'
+import { AnyId } from 'src/types'
+
+type FetchSubstrateProps = {
+  method: string,
+  pallete: string,
+  id: AnyId
+}
+
+export const useGetSubstrateIdsById = <T extends SubstrateId | AccountId = SubstrateId>
+  ({ method, pallete, id }: FetchSubstrateProps): FetchManyResult<T> => {
+    const [ reactionIds, setReactionIds ] = useState<T[]>([])
+    const [ loading, setLoading ] = useState(false)
+  
+    useSubsocialEffect(({ substrate }) => {
+      const loadReactionIds = async () => {
+        setLoading(true)
+        const readyApi = await substrate.api
+  
+        const ids = await readyApi.query[pallete][method](id)
+        console.log(ids)
+  
+        setReactionIds(ids as unknown as T[])
+        setLoading(false)
+      }
+      loadReactionIds()
+    }, [ id ])
+  
+    return {
+      loading,
+      entities: reactionIds
+    }
+}
